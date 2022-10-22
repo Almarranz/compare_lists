d4cd1505fb312fedc2e89642b591bb7535dc7918
Almarranz <83342506+Almarranz@users.noreply.github.com>
2022-10-21 19:14:52 -0700

    Added some parametres
    NOw you have to indicate the index columns where the x and y coordenates are in both lists. It returns two different lists with the common elements of the original lists.

--- compar_lists.py
+++ compar_lists.py
@@ -3,19 +3,22 @@ compare_lists.py
 Almarranz 2021
 Compare coordinates of two lists and extract the rows whose coordinates columns distance are smaller than variable 'distancia'.
 If more than two points in the second list are closer than 'distancia' to a point in the first list,the closest one is choosen 
-Firts two columns of each list have to be coordinates 
+Input parametres: list1, list2, col_index x1, col_index y1, col_index x2, col_index y2, max distance for a positive match
 It computes euclidean distances
 Both lists must be numpy arrays
 """
-def compare_lists(list1,list2,distancia):
-    a='list#1'
-    b='lis#2'
-    diff=[]
+
+def compare_lists(list1,list2,ls1_x,ls1_y,ls2_x,ls2_y,distancia):
+    ls1_com = np.empty((0,list1.shape[1]))
+    ls2_com = np.empty((0,list2.shape[1]))
+    coord1 =np.array([list1[:,ls1_x],list1[:,ls1_y]]).T
+    coord2 =np.array([list2[:,ls2_x],list2[:,ls2_y]]).T
     for i in range(list1.shape[0]): 
-                dist=distance.cdist(list1[i:i+1,0:2],list2[:,0:2], 'euclidean')
+                dist=distance.cdist(coord1[i:i+1,0:2],coord2[:,0:2], 'euclidean')
                 d=np.where(dist<distancia)
                 if len(d[1])>0:
-                    diff.append((list1[i],list2[d[1][np.argmin(dist[d])]]))
+                    ls1_com = np.append(ls1_com,[list1[i],],axis =0)
+                    ls2_com = np.append(ls2_com,[list2[d[1][np.argmin(dist[d])]],],axis =0)
     #dic_listas['stars_1'+str(l)]=diff
-    print('common elements in lists %s and %s ----> '%(a,b),len(diff))
-    return diff
+    print('common elements in lists ----> ',len(ls1_com))
+    return ls1_com,ls2_com
