fffa6850c065fa6659190d4bd72ab71f685cd2dd
Almarranz <83342506+Almarranz@users.noreply.github.com>
2024-05-28 14:31:42 +0200

    Update compar_lists.py
    Compare lists using NN method

--- compar_lists.py
+++ compar_lists.py
@@ -1,52 +1,50 @@
 """
 compare_lists.py
-Almarranz 2021
-Compare coordinates of two lists and extract the rows whose coordinates columns distance are smaller than variable 'distancia'.
-If more than two points in the second list are closer than 'distancia' to a point in the first list,the closest one is choosen 
-Input parametres: list1, list2, col_index x1, col_index y1, col_index x2, col_index y2, max distance for a positive match
-It computes euclidean distances
-Both lists must be numpy arrays
+Almarranz 2024 May 28
+Extracts the nearest neighbors between tow list that are closer that the value of 'dis_min'
+l1 and l2 should be np arrays of coordinates [x,y] 
+Returns: Coord. of l1 matches, Coord. of l2 matches, l1 ind. of matches, l2 ind. of matches, distances 
 """
-from scipy.spatial import distance
+from sklearn.neighbors import NearestNeighbors
 import numpy as np
-import time
-def compare_lists(list1,list2,ls1_x,ls1_y,ls2_x,ls2_y,distancia):
-    ls1_com = np.empty((0,list1.shape[1]))
-    ls2_com = np.empty((0,list2.shape[1]))
-    coord1 =np.array([list1[:,ls1_x],list1[:,ls1_y]]).T
-    coord2 =np.array([list2[:,ls2_x],list2[:,ls2_y]]).T
-    # coord1 =np.array([list1[:,0],list1[:,1]]).T
-    # coord2 =np.array([list2[:,0],list2[:,1]]).T
-    tic1 = time.perf_counter()
-    dist_A = distance.cdist(coord1[:,0:2],coord2[:,0:2], 'euclidean')
-    toc1 = time.perf_counter()
-    print('distance matrix tooks %.2f seconds'%(toc1-tic1))
-    # dist   = distance.cdist(coord1[:,0:2],coord2[:,0:2], 'euclidean')
-    dist = dist_A # for some reason when doing this python also modify dist_A
-    # sys.exit()
-    # dis the is n_elem(l1)*n_elem[l2] matrix. Each row "i" is the distance between
-    # the i-th element in l1 with all the elements in l2
-    l1 =[]
-    l2 =[]
-    # New aproach:
-    #     -Flat and sort the distance matrix
-    #     -Find the position of the first sorted value in the flatted matrix (row, col)
-    #      -If this value is a minimun in its row and column, store both point as 
-    #         a match, mark as inf all values in these rows and columns and move to
-    #         the nex value
-    #      -If not, move to the next sorted distance
-    #     -Repeat till the value of the sorted distance is bigger than min distance
-    marked = np.zeros((dist.shape[0],dist.shape[1]))
-    dist_fs = np.sort(np.reshape(dist_A,dist_A.shape[0]*dist_A.shape[1]))
-    tic = time.perf_counter()
-    for i in range(len(np.where(dist_fs<distancia)[0])):
-        inds = np.argwhere(dist == dist_fs[i])
-        for j in range(len(inds)):
-            if np.isfinite(dist[inds[j][0],inds[j][1]]) and np.isfinite(dist[inds[j][0],inds[j][1]]):
-                if (dist[inds[j][0],inds[j][1]] <= min(dist[inds[j][0]])) and (dist[inds[j][0],inds[j][1]] <= min(dist[:,inds[j][1]])):
-                   dist[inds[j][0]] = float('inf') 
-                   dist[:,inds[j][1]] = float('inf')                   
-                    l1.append(inds[j][0])
-                    l2.append(inds[j][1])
-                    # print(inds[j][0],inds[j][1])
-    return list1[l1],list2[l2], l1, l2 # Common lists and their indices
+def compare_lists(l1,l2, dis_min):
+    nbrs1 = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(l2)
+    dis1, ind1 = nbrs1.kneighbors(l1)
+
+    nbrs2 = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(l1)
+    dis2, ind2 = nbrs2.kneighbors(l2)
+
+    dis1 = np.array(dis1)
+
+    # print(ind1)
+
+    # print(ind2)
+    matc1  = []
+    matc2  = []
+    d1 = [] 
+    d2 = [] 
+    l1_i = []
+    l2_i = []
+    for i, idx1 in enumerate(ind1):
+        # idx1 is the index of the nearest neighbor in list2 for point i in list1
+        if ind2[idx1[0]] == i and dis1[i]< dis_min:
+            # print(l1[i], l2[idx1[0]])
+            matc1.append(l1[i])
+            matc2.append(l2[idx1[0]])
+            d1.append(dis1[i])
+            d2.append(dis2[idx1[0]])
+            l1_i.append(i)
+            l2_i.append(idx1[0])
+            
+
+    # valid = d1 < dis_min       
+    d1 = np.array(d1)
+    d2 = np.array(d2)
+    matc1= np.array(matc1)
+    matc2= np.array(matc2)
+    l1_i = np.array(l1_i)
+    l2_i = np.array(l2_i)
+
+    comp = np.c_[matc1,matc2,l1_i,l2_i,d1]
+    
+    return comp
